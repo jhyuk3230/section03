4595d6935de0755807bf8717fdad9168b4825199
jhyuk3230 <jhyuk3230@naver.com>
2024-11-12 16:37:24 +0900

    에러 컨트롤

---
+++ src/app/error.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function Error({error,reset}: {error:Error; reset:()=>void}){
+
+	const router = useRouter();
+
+	useEffect(()=>{
+		console.error(error.message);
+	}, [error]);
+
+	return (
+		<div>
+			<p>에러</p>
+			<button onClick={() => {
+				startTransition(()=>{
+					router.refresh();
+					reset();
+				})
+			}}>다시 시도</button>
+		</div>
+	)
+}
